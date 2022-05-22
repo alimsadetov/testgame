693b2c26ccb16351358854dd0f74366e63660317
alimsadetov <alimsadetov2019@mail.ru>
2022-05-22 15:39:10 +0300

    init

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1653152907,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tilemap.png",
+		"images/hero_standart-sheet0.png",
+		"images/hero_standart-sheet1.png",
+		"images/hero_dzabudza-sheet0.png",
+		"images/hero_dzabudza-sheet1.png",
+		"images/hero_dzabudza-sheet2.png",
+		"images/tiledbackground.png",
+		"images/my_hp-sheet0.png",
+		"images/my_hp_background-sheet0.png",
+		"images/my_def-sheet0.png",
+		"images/dzabudza_ultimate-sheet0.png",
+		"images/energy-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"Photon-Javascript_SDK.min.js"
+	]
+}
